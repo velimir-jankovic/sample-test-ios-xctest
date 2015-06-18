8999eb577ec1d1cc7823ce77ae79ae12661c3578
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-06-18 20:35:36 +0200

    Added network communication

--- BitriseTestingSample/BitriseTestingSample/ViewController.swift
+++ BitriseTestingSample/BitriseTestingSample/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func testButtonTouched(sender: AnyObject) {
+        testLabel.hidden = true;
+        let urlPath: String = "http://google.com"
+        var url: NSURL = NSURL(string: urlPath)!
+        var request1: NSURLRequest = NSURLRequest(URL: url)
+        let queue:NSOperationQueue = NSOperationQueue()
+        NSURLConnection.sendAsynchronousRequest(request1, queue: queue, completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var err: NSError
+            let httpResponse = response as? NSHTTPURLResponse
+            let responseStatus = httpResponse!.statusCode;
+            if(responseStatus == 200)
+            {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.testLabel.text = "Great Success! We got status " + String(stringInterpolationSegment: responseStatus) + (" from the server! Nice job!");
+                    self.testLabel.hidden = false;
+                }
+            }
+            else
+            {
+                dispatch_async(dispatch_get_main_queue()) {
+                self.testLabel.text = "Oh no! We got status " + String(stringInterpolationSegment: responseStatus) + (" from the server!");
+                self.testLabel.hidden = false;
+                }
+            }
+        })
+    }
 }
 
