981e003d5fa07216afb881ec8b3dd8266e89c558
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-06-19 15:25:56 +0200

    Reversed commit

--- BitriseTestingSample/BitriseTestingSample/ViewController.swift
+++ BitriseTestingSample/BitriseTestingSample/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,26 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func testButtonTouched(sender: AnyObject) {
-    }
-    
-    static func pingServer() -> Bool {
-        let urlPath: String = "http://google.com"
-        var url: NSURL = NSURL(string: urlPath)!
-        var request1: NSURLRequest = NSURLRequest(URL: url)
-        var response: NSURLResponse?
-        var error: NSError?
-        let urlData = NSURLConnection.sendSynchronousRequest(request1, returningResponse: &response, error: &error)
-        if let httpResponse = response as? NSHTTPURLResponse {
-            if(httpResponse.statusCode == 200)
-            {
-                return true
-            } else {
-                return false
-            }
-        }
-        return false
-    }
+
+
 }
 
